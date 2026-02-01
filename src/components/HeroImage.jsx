64fdcd37b0cad81cdf393ec561244f30de588bec
deepanshusaxena2127-commits <deepanshusaxena2127@gmail.com>
2026-02-01 23:18:49 +0530

    Update HeroImage component with new image and attributes

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 const HeroImage = () => {
   return (
-    <div className='flex justify-center items-center'>
-      <img 
-        src='https://imagedelivery.net/LqiWLm-3MGbYHtFuUbcBtA/119580eb-abd9-4191-b93a-f01938786700/public' 
-        alt='Hostinger Horizons' 
+    <div className="flex justify-center items-center w-full">
+      <img
+        src="https://imagedelivery.net/LqiWLm-3MGbYHtFuUbcBtA/119580eb-abd9-4191-b93a-f01938786700/public"
+        alt="MillenniumAi global AI data and engineering visualization"
+        loading="lazy"
+        className="max-w-full h-auto rounded-xl"
       />
     </div>
   );
